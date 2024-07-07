d12b9fdcd3e91fe155d7f7ddd07bfecfb56739dc
krivobokov <krivobokov_sv@magnit.ru>
2024-07-07 14:14:03 +0300

    update user validator test fix

--- app/tests/CreateUserValidatorTest.php
+++ app/tests/CreateUserValidatorTest.php
@@ -9,7 +9,11 @@ use Test\Mock\TestAllowedDomainsStrategy;
 use Test\Mock\TestDeniedWordsStrategy;
 use Test\Mock\TestUserRepository;
 use User\Dto\User;
+use User\Exception\EmailDomainIsNotAllowedException;
+use User\Exception\EmailIsNotValidException;
 use User\Exception\OnlyEmptyIdIsPermittedForCreateException;
+use User\Exception\TooShortNameException;
+use User\Exception\WordIsDeniedException;
 use User\Validator\CreateUserValidator;
 
 class CreateUserValidatorTest extends TestCase
@@ -59,5 +63,119 @@ class CreateUserValidatorTest extends TestCase
         );
 
         $this->assertTrue($validator->validate($user));
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testShortNameCreate(): void
+    {
+        $user = new User(null, '1', '1', '1');
+
+        $repository = new TestUserRepository();
+        $deniedWordsStrategy = new TestDeniedWordsStrategy();
+        $allowedDomainsStrategy = new TestAllowedDomainsStrategy();
+
+        $validator = new CreateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+        $this->expectExceptionObject(new TooShortNameException());
+        $validator->validate($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testInvalidEmailCreate(): void
+    {
+        $user = new User(null, '1111111111', 'test', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $deniedWordsStrategy = new TestDeniedWordsStrategy();
+        $allowedDomainsStrategy = new TestAllowedDomainsStrategy();
+
+        $validator = new CreateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+
+        $this->expectExceptionObject(new EmailIsNotValidException());
+        $validator->validate($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testDeniedWordsCreate(): void
+    {
+        $user = new User(null, '1111111111', 'test@test.ru', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $deniedWordsStrategy = $this->createMock(TestDeniedWordsStrategy::class);
+        $deniedWordsStrategy
+            ->expects($this->any())
+            ->method('isValid')
+            ->willReturnOnConsecutiveCalls(false);
+
+        $allowedDomainsStrategy = new TestAllowedDomainsStrategy();
+
+        $validator = new CreateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+
+        $this->expectExceptionObject(new WordIsDeniedException());
+        $validator->validate($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testAllowedEmailDomainCreate(): void
+    {
+        $user = new User(null, '1111111111', 'test@test.ru', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $deniedWordsStrategy = new TestDeniedWordsStrategy();
+
+        $allowedDomainsStrategy = $this->createMock(TestAllowedDomainsStrategy::class);
+        $allowedDomainsStrategy
+            ->expects($this->any())
+            ->method('isValid')
+            ->willReturnOnConsecutiveCalls(false);
+
+        $validator = new CreateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+
+        $this->expectExceptionObject(new EmailDomainIsNotAllowedException());
+        $validator->validate($user);
     }
 }
--- app/tests/UpdateUserValidatorTest.php
+++ app/tests/UpdateUserValidatorTest.php
@@ -10,7 +10,12 @@ use Test\Mock\TestDeniedWordsStrategy;
 use Test\Mock\TestUserRepository;
 use User\Dto\User;
 use User\Entity\User as DbUser;
+use User\Exception\EmailDomainIsNotAllowedException;
+use User\Exception\EmailIsNotValidException;
 use User\Exception\OnlyNotEmptyIdIsPermittedForUpdateException;
+use User\Exception\TooShortNameException;
+use User\Exception\WordIsDeniedException;
+use User\Validator\CreateUserValidator;
 use User\Validator\UpdateUserValidator;
 
 class UpdateUserValidatorTest extends TestCase
@@ -74,5 +79,154 @@ class UpdateUserValidatorTest extends TestCase
         );
 
         $this->assertTrue($validator->validate($user));
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testShortNameUpdate(): void
+    {
+        $user = new User(1, '1', '1', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $repository
+            ->expects($this->any())
+            ->method('findById')
+            ->willReturnOnConsecutiveCalls(
+                new DbUser(),
+            );
+
+        $deniedWordsStrategy = new TestDeniedWordsStrategy();
+        $allowedDomainsStrategy = new TestAllowedDomainsStrategy();
+
+        $validator = new UpdateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+        $this->expectExceptionObject(new TooShortNameException());
+        $validator->validate($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testInvalidEmailUpdate(): void
+    {
+        $user = new User(1, '1111111111', 'test', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $repository
+            ->expects($this->any())
+            ->method('findById')
+            ->willReturnOnConsecutiveCalls(
+                new DbUser(),
+            );
+
+        $deniedWordsStrategy = new TestDeniedWordsStrategy();
+        $allowedDomainsStrategy = new TestAllowedDomainsStrategy();
+
+        $validator = new UpdateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+
+        $this->expectExceptionObject(new EmailIsNotValidException());
+        $validator->validate($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testDeniedWordsUpdate(): void
+    {
+        $user = new User(1, '1111111111', 'test@test.ru', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $repository
+            ->expects($this->any())
+            ->method('findById')
+            ->willReturnOnConsecutiveCalls(
+                new DbUser(),
+            );
+
+        $deniedWordsStrategy = $this->createMock(TestDeniedWordsStrategy::class);
+        $deniedWordsStrategy
+            ->expects($this->any())
+            ->method('isValid')
+            ->willReturnOnConsecutiveCalls(false);
+
+        $allowedDomainsStrategy = new TestAllowedDomainsStrategy();
+
+        $validator = new UpdateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+
+        $this->expectExceptionObject(new WordIsDeniedException());
+        $validator->validate($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testAllowedEmailDomainUpdate(): void
+    {
+        $user = new User(1, '1111111111', 'test@test.ru', '1');
+
+        $repository = $this->createMock(TestUserRepository::class);
+        $repository
+            ->expects($this->any())
+            ->method('find')
+            ->willReturnOnConsecutiveCalls(
+                null,
+            );
+
+        $repository
+            ->expects($this->any())
+            ->method('findById')
+            ->willReturnOnConsecutiveCalls(
+                new DbUser(),
+            );
+
+        $deniedWordsStrategy = new TestDeniedWordsStrategy();
+
+        $allowedDomainsStrategy = $this->createMock(TestAllowedDomainsStrategy::class);
+        $allowedDomainsStrategy
+            ->expects($this->any())
+            ->method('isValid')
+            ->willReturnOnConsecutiveCalls(false);
+
+        $validator = new UpdateUserValidator(
+            $repository,
+            $deniedWordsStrategy,
+            $allowedDomainsStrategy,
+        );
+
+        $this->expectExceptionObject(new EmailDomainIsNotAllowedException());
+        $validator->validate($user);
     }
 }
